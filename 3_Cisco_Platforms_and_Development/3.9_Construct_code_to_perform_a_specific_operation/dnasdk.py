e7770c3b1afd710792aeae908329f30423e050a9
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-06-20 20:48:17 +0530

    dnasdk

--- 3_Cisco_Platforms_and_Development/3.9_Construct_code_to_perform_a_specific_operation/dnasdk.py
+++ 3_Cisco_Platforms_and_Development/3.9_Construct_code_to_perform_a_specific_operation/dnasdk.py
@@ -9,40 +9,39 @@ dna = api.DNACenterAPI(username="devnetuser", password="Cisco123!",
 
 ##### NETWORKS AND SITES ####
 
-# # Print Site Topology
-# sites = dna.networks.get_site_topology()
-# for site in sites.response.sites:
-#     for child_sites in sites.response.sites:
-#         if child_sites.parentId == site.id:
-#             print(f'{site.name} -> {child_sites.name}')
-#         for more_children in sites.response.sites:
-#             if more_children.parentId == child_sites.id and child_sites.parentId == site.id:
-#                 print(f'{site.name} -> {child_sites.name} -> {more_children.name}')
+# Print Site Topology
+sites = dna.networks.get_site_topology()
+for site in sites.response.sites:
+    for child_sites in sites.response.sites:
+        if child_sites.parentId == site.id:
+            print(f'{site.name} -> {child_sites.name}')
+        for more_children in sites.response.sites:
+            if more_children.parentId == child_sites.id and child_sites.parentId == site.id:
+                print(f'{site.name} -> {child_sites.name} -> {more_children.name}')
 
-# # Print Vlans
-# vlans = dna.networks.get_vlan_details()
-# for vlan in vlans.response:
-#     print(vlan)
+# Print Vlans
+vlans = dna.networks.get_vlan_details()
+for vlan in vlans.response:
+    print(vlan)
 
-# # Physical Topology Details
-# phys_top = dna.networks.get_physical_topology()
-# print(json.dumps(phys_top, indent=2, sort_keys=True))
+# Physical Topology Details
+phys_top = dna.networks.get_physical_topology()
+print(json.dumps(phys_top, indent=2, sort_keys=True))
 
 
-# ############### DEVICES #############
+############### DEVICES #############
+# Print Device Details
+devices = dna.devices.get_device_list()
+for device in devices.response:
+    print(device.type)
+    print(device.hostname)
+    print(device.managementIpAddress)
+    print(device.id)
+    print(" ")
 
-# # Print Device Details
-# devices = dna.devices.get_device_list()
-# for device in devices.response:
-#     print(device.type)
-#     print(device.hostname)
-#     print(device.managementIpAddress)
-#     print(device.id)
-#     print(" ")
-
-# # Get a specific device
-# device = dna.devices.get_device_by_id('72dc1f0a-e4da-4ec3-a055-822416894dd5')
-# pprint(device)
+# Get a specific device
+device = dna.devices.get_device_by_id('72dc1f0a-e4da-4ec3-a055-822416894dd5')
+pprint(device)
 
 
 ######## HEALTH CHECKS ################
